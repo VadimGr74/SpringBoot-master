1d18bf3e6ea6f49a043b6ba049f6531bd505f70a
VadimGr74 <161434080+VadimGr74@users.noreply.github.com>
2025-01-27 18:33:06 +0100

    commit123

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        //main alo
+        //main alo123
         SpringApplication.run(DemoApplication.class, args);
     }
 }
